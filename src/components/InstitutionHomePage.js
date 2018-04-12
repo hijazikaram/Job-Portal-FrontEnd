883becd9d3ffe015596c8eb4c1a3b6538a9ed499
webmas216 <tyefred210@gmail.com>
2018-04-12 10:47:59 -0500

    Fixed Task 8

--- src/components/InstitutionHomePage.js
+++ src/components/InstitutionHomePage.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
 import userImg from '../img/user.jpg';
+import axios from 'axios';
 
 class UserHomePage extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      name : '',
+      email : '',
+      phoneNumber: '',
+      address: '',
+      type: '',
+      comments_enable : false,
+      receive_newsletter: false,
+      receive_advice: false,
+
+      oldPassword: '',
+      newPassword: '',
+
+      notificationMsg : '',
+      updateProfileValid: false
+    };
+
+    this.oldPassword = '';
+    this.newPassword = '';
+    this.newPasswordConfirm = '';
+
+    this.id = localStorage.getItem('user_id');
+
+    axios.get('http://localhost:5000/api/institution/' + this.id).then(res => {
+      var institution = res.data.institution;
+      this.setState({ name : institution.name, email : institution.email ,phoneNumber : institution.phoneNumber, comments_enable: institution.comments_enable, receive_newsletter: institution.receive_newsletter, receive_advice: institution.receive_advice});
+
+      if(!institution.address) {
+        this.setState({ address : '' });
+      } else {
+        this.setState({ address : institution.address });
+      }
+
+      if(!institution.type) {
+        this.setState({ type : '' });
+      } else {
+        this.setState({ type : institution.type });
+      }
+    });
   }
 
   componentWillMount() {
@@ -14,8 +55,111 @@ class UserHomePage extends Component {
       window.location.href = '/SignIn';
     }
   }
+  onUserNameChange(e) {
+    this.setState({ name : e.target.value });
+  }
+
+  onEmailChange(e) {
+    this.setState({ email : e.target.value });
+  }
+
+  onPhoneNumberChange(e) {
+    this.setState({ phoneNumber : e.target.value });
+  }
+
+  onAddressChange(e) {
+    this.setState({ address : e.target.value });
+  }
+
+  onTypeChange(e){
+    this.setState({ type : e.target.value });
+  }
+
+  onUpdateProfile(e) {
+    e.preventDefault();
+    var self = this;
+
+    if(self.oldPassword !='') {
+      if(!self.newPassword || !self.newPasswordConfirm) {
+        // Must input new password & confirm password
+        self.setState({ notificationMsg: 'Please input new Password' , updateProfileValid : false });
+      } else {
+        // New Password request here.
+        if(self.newPassword != self.newPasswordConfirm) {
+          self.setState({ notificationMsg: 'New Password doesnt match' , updateProfileValid : false });
+        } else {
+          self.setState({ notificationMsg: '' , updateProfileValid : false, oldPassword : self.oldPassword, newPassword : self.newPassword }, () => {
+            axios.put('http://localhost:5000/api/institution/' + self.id , self.state).then(function (response) {
+              console.log(response);
+              if(!response.data.error) {
+                self.setState({ notificationMsg: 'Updated Successfully.' , updateProfileValid : true });
+              } else {
+                self.setState({ notificationMsg: response.data.error , updateProfileValid : false });
+              }
+            }, function (error) {
+              console.log(error);
+            });
+          });
+        }
+      }
+    } else {
+      axios.put('http://localhost:5000/api/institution/' + self.id , self.state).then(function (response) {
+        console.log(response);
+        if(!response.data.error) {
+          self.setState({ notificationMsg: 'Updated Successfully.' , updateProfileValid : true });
+        }
+      }, function (error) {
+        console.log(error);
+      });
+    }
+  }
+
+  onUpdateCommentsEnable(e) {
+    this.setState({ comments_enable : e.target.checked });
+  }
+
+  onUpdateReceiveNewsletter(e) {
+    this.setState({ receive_newsletter : e.target.checked });
+  }
+
+  onUpdateReceiveAdvice(e) {
+    this.setState({ receive_advice : e.target.checked });
+  }
+
+  onOldPasswordInput(e) {
+    this.oldPassword = e.target.value;
+  }
+
+  onNewPasswordChange(e) {
+    this.newPassword = e.target.value; 
+  }
+
+  onNewPasswordConfirmChange(e) {
+    this.newPasswordConfirm = e.target.value;
+  }
   render() {
-    console.log("render");
+    var userNameChange = this.onUserNameChange.bind(this);
+    var emailChange = this.onEmailChange.bind(this);
+    var phoneNumberChange = this.onPhoneNumberChange.bind(this);
+    var addressChange = this.onAddressChange.bind(this);
+    var typeChange = this.onTypeChange.bind(this);
+
+    var oldPasswordInput = this.onOldPasswordInput.bind(this);
+    var newPasswordChange = this.onNewPasswordChange.bind(this);
+    var newPasswordConfirmChange = this.onNewPasswordConfirmChange.bind(this);
+
+    var updateCommentsEnable = this.onUpdateCommentsEnable.bind(this);
+    var updateReceiveNewsletter = this.onUpdateReceiveNewsletter.bind(this);
+    var updateReceiveAdvice = this.onUpdateReceiveAdvice.bind(this);
+
+    var updateProfile = this.onUpdateProfile.bind(this);
+    
+    var notification = this.state.notificationMsg ? (
+      <div className='panel panel-default'>
+        <div className={`notification ${!this.state.updateProfileValid ? 'error' : 'success'}`}>{ this.state.notificationMsg }
+        </div>
+      </div>) : (<div></div>);
+
     return (
       <div>
         <div className="breadcrumb-section">
@@ -23,76 +167,92 @@ class UserHomePage extends Component {
     				<div className="user-pro-section">
     					<div className="profile-details section">
     						<h2>Profile Details</h2>
+
+                {notification}
+
     						<form action="#">
     							<div className="form-group">
     								<label>Username</label>
-    								<input type="text" className="form-control" placeholder="Jhon Doe"/>
+    								<input type="text" className="form-control" placeholder="Jhon Doe" value={this.state.name} onChange={userNameChange}/>
     							</div>
 
     							<div className="form-group">
     								<label>Email ID</label>
-    								<input type="email" className="form-control" placeholder="jhondoe@mail.com"/>
+    								<input type="email" className="form-control" placeholder="jhondoe@mail.com"  value={this.state.email} onChange={emailChange}/>
     							</div>
 
     							<div className="form-group">
     								<label>Mobile</label>
-    								<input type="text" className="form-control" placeholder="+213 1234 56789"/>
+    								<input type="text" className="form-control" placeholder="+213 1234 56789" value={this.state.phoneNumber} onChange={phoneNumberChange}/>
     							</div>
 
     							<div className="form-group">
     								<label>Your City</label>
-    								<select className="form-control">
-    									<option value="#">Los Angeles, USA</option>
-    									<option value="#">Dhaka, BD</option>
-    									<option value="#">Shanghai</option>
-    									<option value="#">Karachi</option>
-    									<option value="#">Beijing</option>
-    									<option value="#">Lagos</option>
-    									<option value="#">Delhi</option>
-    									<option value="#">Tianjin</option>
-    									<option value="#">Rio de Janeiro</option>
+    								<select className="form-control" value={this.state.address} onChange={addressChange}>
+                      <option value="">Select the City</option>
+    									<option value="Los_angeles">Los Angeles, USA</option>
+    									<option value="Dhaka">Dhaka, BD</option>
+    									<option value="Shanghai">Shanghai</option>
+    									<option value="Karachi">Karachi</option>
+    									<option value="Beijing">Beijing</option>
+    									<option value="Lagos">Lagos</option>
+    									<option value="Delhi">Delhi</option>
+    									<option value="Tianjin">Tianjin</option>
+    									<option value="Rio">Rio de Janeiro</option>
     								</select>
     							</div>
 
     							<div className="form-group">
     								<label>You are a</label>
-    								<select className="form-control">
-    									<option value="#">Employer</option>
-    									<option value="#">Employee</option>
+    								<select className="form-control" value={this.state.type} onChange={typeChange}>
+                      <option value="">Select the type</option>
+    									<option value="Employer">Employer</option>
+    									<option value="Employee">Employee</option>
     								</select>
     							</div>
     						</form>
     					</div>
     					<div className="change-password section">
-    						<h2>Change password</h2>
-    						<div className="form-group">
-    							<label>Old Password</label>
-    							<input type="password" className="form-control" />
-    						</div>
+                <h2>Change password</h2>
+                <div className="form-group">
+                  <label>Old Password</label>
+                  <input type="password" className="form-control" onChange={oldPasswordInput} />
+                </div>
 
-    						<div className="form-group">
-    							<label>New password</label>
-    							<input type="password" className="form-control"/>
-    						</div>
+                <div className="form-group">
+                  <label>New password</label>
+                  <input type="password" className="form-control" onChange={newPasswordChange}/>
+                </div>
 
-    						<div className="form-group">
-    							<label>Confirm password</label>
-    							<input type="password" className="form-control"/>
-    						</div>
-    					</div>
+                <div className="form-group">
+                  <label>Confirm password</label>
+                  <input type="password" className="form-control" onChange={newPasswordConfirmChange}/>
+                </div>
+              </div>
     					<div className="preferences-settings section">
-    						<h2>Preferences Settings</h2>
-    						<div className="checkbox">
-    							<label><input type="checkbox" name="logged"/>Comments are enabled on my Resume</label>
-    							<label><input type="checkbox" name="receive"/>I want to receive newsletter.</label>
-    							<label><input type="checkbox" name="want"/>I want to receive advice on portfolio</label>
-    						</div>
+                <h2>Preferences Settings</h2>
+                <div className="checkbox">
+                  <label className ={`${this.state.comments_enable ? 'checked' : ''}`}>
+                    <input type="checkbox" name="logged" checked={this.state.comments_enable} onChange={updateCommentsEnable}/>
+                    Comments are enabled on my Resume
+                  </label>
 
-    						<div className="buttons">
-    							<a href="#" className="btn">Update Profile</a>
-    							<a href="#" className="btn cancle">Cancel</a>
-    						</div>
-    					</div>
+                  <label className ={`${this.state.receive_newsletter ? 'checked' : ''}`}>
+                    <input type="checkbox" name="receive" checked={this.state.receive_newsletter} onChange={updateReceiveNewsletter}/>
+                    I want to receive newsletter.
+                  </label>
+
+                  <label className ={`${this.state.receive_advice ? 'checked' : ''}`}>
+                    <input type="checkbox" name="want" checked={this.state.receive_advice} onChange={updateReceiveAdvice}/>
+                    I want to receive advice on portfolio
+                  </label>
+                </div>
+
+                <div className="buttons">
+                  <a href="javascript:void(0)" className="btn" onClick={updateProfile}>Update Profile</a>
+                  <a href="#" className="btn cancle">Cancel</a>
+                </div>
+              </div>
     				</div>
           </div>
         </div>
